67a5f00dbd8b70555f44a68541c450d7c5618c24
PoojithaK0108 <poojitha.korrapati@gmail.com>
2025-03-10 08:55:43 +0530

    10/03/25 - Made some changes in FrizzBuzz practice program

--- src/ex_01_Java_Basic/FrizzBuzz.java
+++ src/ex_01_Java_Basic/FrizzBuzz.java
@@ -3,11 +3,11 @@ package ex_01_Java_Basic;
 public class FrizzBuzz {
     public static void main(String[] args) {
         for(int i=1;i<=100;i++){
-            if (i%3==0 & i%5 ==0)
+            if (i%5==0 & i%7 ==0)
                 System.out.println("FrizzBuzz");
-            else if (i%3==0) {
+            else if (i%5==0) {
                 System.out.println("Frizz");
-            } else if (i%5 ==0) {
+            } else if (i%7 ==0) {
                 System.out.println("Buzz");
             }
             else {
