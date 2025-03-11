e2a21cfcff8f1d87977fe6cb97623922573c7dae
PoojithaK0108 <poojitha.korrapati@gmail.com>
2025-03-11 22:21:57 +0530

    11/03/25 - Nine Table practice program

---
+++ src/ex_02_Java_Basic_Part2/Lab014_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basic_Part2;
+
+public class Lab014_Variables {
+    public static void main(String[] args) {
+        int p = 20 ;
+        p = 20 + 80 ;
+        System.out.println(p);
+    }
+}
---
+++ src/ex_02_Java_Basic_Part2/NineTable.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basic_Part2;
+
+public class NineTable {
+    public static void main(String[] args) {
+        for(int i=1;i <= 10 ;i++){
+            System.out.print("9 * "+ i + "=" + (9 * i));
+            System.out.println();
+        }
+    }
+}
